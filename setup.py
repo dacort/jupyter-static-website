5d8903eb02370cd83a1243306e51fea36bcc4751
Damon P. Cortesi <d.lifehacker@gmail.com>
2021-07-14 14:21:27 -0700

    Update description in setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     name="emr_studio_publisher",
     version="0.0.1",
 
-    description="An empty CDK Python app",
+    description="Jupyter Notebook CD Pipeline",
     long_description=long_description,
     long_description_content_type="text/markdown",
 
